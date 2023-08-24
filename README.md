1422275743bcbc690e78b76ad5b2e3f517f323da
sik2 <devpang20@gmail.com>
2023-08-24 21:23:20 +0900

    api 연동

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 포켓몬 도감 앱 만들기 교보재
 
 ## TODO
-- [ ] fetch api 연결
+- [x] fetch api 연결
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [loding, setloading] = useState(true)
+  const [pokemons, setPokemons] = useState([])
+
+  //api "https://pokeapi.co/api/v2/pokemon"
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then(reponse => reponse.json())
+      .then(data => {
+          setPokemons(data.results)
+          setloading(false)
+        }
+      )
+  }, [])
+
+  if ( loding ) {
+    return "로딩 중..."
+  }
+
 
   return (
     <>
-      <h1>포켓몬 도감</h1>
+      <ul>
+        {pokemons.map((pokemon, index) => (<li key={index}>{pokemon.name}</li>))} 
+      </ul> 
     </>
   )
 }
