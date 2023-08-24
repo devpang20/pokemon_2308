64bf1142dace8452e05d079296f82b939fb030d6
sik2 <devpang20@gmail.com>
2023-08-24 21:44:24 +0900

    이전 이후 페이지 이동 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,45 @@ function App() {
 
   const [loding, setloading] = useState(true)
   const [pokemons, setPokemons] = useState([])
+  const [offset, setOffset] = useState(0)
+  const totalCount = 1281;
+  const limit = 30
+
+  const showPrev = () => {
+    setOffset(offset - limit)
+  }
+  const showNext = () => {
+    setOffset(offset + limit)
+  }
 
   //api "https://pokeapi.co/api/v2/pokemon"
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    setloading(true)
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
       .then(reponse => reponse.json())
       .then(data => {
           setPokemons(data.results)
           setloading(false)
         }
       )
-  }, [])
+      .catch((err) => console.log("err", err))
+  }, [offset])
 
   if ( loding ) {
     return "로딩 중..."
   }
 
-
   return (
     <>
+      offset: {offset}
+      { offset > 0 && <button onClick={showPrev}>이전</button> }
+
       <ul>
         {pokemons.map((pokemon, index) => (<li key={index}>{pokemon.name}</li>))} 
       </ul> 
+      { totalCount > offset + limit && <button onClick={showNext}>이후</button> }
+      
     </>
   )
 }
